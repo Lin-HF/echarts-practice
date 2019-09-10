9a0a02c7e91a9e3a6e17fd55b198f524ad4aa33f
David Lin <haiflin@iu.edu>
2019-09-10 14:22:33 -0700

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import Bar from './echarts/bar';
-import Pie from './echarts/pie';
-import Line from './echarts/line';
-import Graph from './echarts/graph';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Bar from "./echarts/bar";
+import Pie from "./echarts/pie";
+import Line from "./echarts/line";
+import Graph from "./echarts/graph";
+import { NavLink, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Bar/>
-        <Pie/>
-        <Line/>
-        <Graph/>
+        <nav>
+          <ul style={{ display: "inline-block" }}>
+            <li>
+              <NavLink to="/bar">Bar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/pie">Pie</NavLink>
+            </li>
+            <li>
+              <NavLink to="/line">Line</NavLink>
+            </li>
+            <li>
+              <NavLink to="/graph">Graph</NavLink>
+            </li>
+            <li>
+              <NavLink to="/graph">Http</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/bar" component={Bar} />
+          <Route path="/pie" component={Pie} />
+          <Route path="/line" component={Line} />
+          <Route path="/graph" component={Graph} />
+          <Route
+            path="/"
+            render={() => (
+              <div>
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                  To get started, edit <code>src/App.js</code> and save to
+                  reload.
+                </p>
+              </div>
+            )}
+          />
+        </Switch>
+
+        {/* <Bar />
+        <Pie />
+        <Line />
+        <Graph /> */}
       </div>
     );
   }
